ef176eefe07f8b3e651c9b768906a81d2894e1b9
apaspxp <63920754+apaspxp@users.noreply.github.com>
2023-07-12 17:21:58 +0530

    Update JenkinsController.java

--- src/main/java/com/waterco/jenkinsdemo/controller/JenkinsController.java
+++ src/main/java/com/waterco/jenkinsdemo/controller/JenkinsController.java
@@ -1,6 +1,7 @@
 package com.waterco.jenkinsdemo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
