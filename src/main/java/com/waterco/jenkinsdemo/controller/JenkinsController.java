98c33e9f1d8775070d802613497818cf1171d035
rajesh1420621 <panigrahirajesh578@gmail.com>
2023-07-16 16:44:47 +0530

    Removed the docker agent in jenkins file

--- src/main/java/com/waterco/jenkinsdemo/controller/JenkinsController.java
+++ src/main/java/com/waterco/jenkinsdemo/controller/JenkinsController.java
@@ -7,22 +7,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JenkinsController {
 
-	@RequestMapping("hellojenkins")
-	public String hello() {
-		return "Hello from Jenkins Controller class.";
-	}
-	
-	@RequestMapping(value = "byejenkins", method = RequestMethod.GET)
-	public String bye() {
-		return "Bye from Jenkins Controller class.";
-	}
-	
-	@RequestMapping("testjenkins")
-	public String testJenkins() {
-		return "Test from Jenkins Controller class. Adding some more text for demo second time...";
-	}
-	@RequestMapping(value = "buildjenkins", method = RequestMethod.GET)
-	public String buildJenkins() {
-		return "Build jenkins from Jenkins Controller class....";
-	}
+    @RequestMapping(value = "hellojenkins", method = RequestMethod.GET)
+    public String hello() {
+        return "Hello from Jenkins Controller class.";
+    }
+
+    @RequestMapping(value = "byejenkins", method = RequestMethod.GET)
+    public String bye() {
+        return "Bye from Jenkins Controller class.";
+    }
+
+    @RequestMapping(value = "testjenkins", method = RequestMethod.GET)
+    public String testJenkins() {
+        return "Test from Jenkins Controller class. Adding some more text for demo second time.";
+    }
+
+    @RequestMapping(value = "buildjenkins", method = RequestMethod.GET)
+    public String buildJenkins() {
+        return "Build jenkins from Jenkins Controller class.";
+    }
 }
